8bfd287cf509636fe9a286a5ff9bf07fbc9b97fe
Jaime Pérez Castellanosç <jaipecas89@gmail.com>
2022-12-25 11:32:18 +0100

    pruebas webAPI

--- webAPI/Controllers/WeatherForecastController.cs
+++ webAPI/Controllers/WeatherForecastController.cs
@@ -6,28 +6,17 @@ namespace webAPI.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        [HttpGet]
+        public IEnumerable<string> Get()
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            string[] nombres = new[]
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                "Fabian",
+                "Rolando",
+                "María",
+                "Rebeca"
+            };
+            return nombres;
         }
     }
 }
