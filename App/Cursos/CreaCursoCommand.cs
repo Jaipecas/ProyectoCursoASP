8512192e95894fb0ff5bb1cd671e4eef9d3d1825
Jaime Pérez Castellanosç <jaipecas89@gmail.com>
2023-01-08 00:42:21 +0100

    Se cambia el command createCurso

--- App/Cursos/CreaCursoCommand.cs
+++ App/Cursos/CreaCursoCommand.cs
@@ -10,6 +10,7 @@ namespace App.Cursos
         public string? titulo { get; set; }
         public string? descripcion { get; set; }
         public DateTime? fechaPublicacion { get; set; }
+        public List<Guid> instructores { get; set; }
     }
 
     public class CrearCursoValidations : AbstractValidator<CreaCurso>
@@ -31,14 +32,31 @@ namespace App.Cursos
         public async Task<Unit> Handle(CreaCurso request, CancellationToken cancellationToken)
         {
             int changes;
+            Guid cursoId = new Guid();
+
             Curso curso = new()
             {
+                cursoId = cursoId,
                 titulo = request.titulo,
                 descripcion = request.descripcion,
                 fechaPublicacion = request.fechaPublicacion,
             };
 
             _context.Curso.Add(curso);
+
+            if (request.instructores != null)
+            {
+                request.instructores.ForEach(instructorId =>
+                {
+                    var cursoInstructor = new CursoInstructor
+                    {
+                        cursoId = curso.cursoId,
+                        instructorId = instructorId
+                    };
+                    _context.cursoInstructor.Add(cursoInstructor);
+                });
+            }
+    
             changes = await _context.SaveChangesAsync();
 
             if (changes > 0) return Unit.Value;
